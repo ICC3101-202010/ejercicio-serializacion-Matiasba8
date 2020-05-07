178aac034d99f7db8f8c0e45a6f8e748b1d3746b
MatiBa8 <mibustos2@miuandes.cl>
2020-05-07 10:22:46 -0400

    firstcommit

---
+++ Serializacion/Persona.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacion
+{
+    [Serializable]
+    class Persona : ISerializable
+    {
+        private string nombre;
+        private string apellido;
+        private string edad;
+
+        //Lista persona
+        private List<Persona> ListaPersona = new List<Persona>();
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Edad { get => edad; set => edad = value; }
+
+        public Persona(string nombre, string apellido, string edad)
+        {
+            this.Nombre = nombre;
+            this.Nombre = apellido;
+            this.Nombre = edad;
+
+        }
+        public Persona()
+        {
+
+        }
+        public void AddPerson(Persona persona)
+        {
+            ListaPersona.Add(persona);
+        }
+        public void ShowListPerson()
+        {
+            foreach (Persona persona in ListaPersona)
+            {
+                Console.WriteLine("Name: " + persona.nombre + " LastName:" + persona.Apellido + " Edad:" + persona.Edad);
+            }
+        }
+        public List<Persona> ReturnListPerson()
+        {
+            return ListaPersona;
+        }
+
+        //Serializacion
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name:", Nombre);
+            info.AddValue("LatName:", Apellido);
+            info.AddValue("Age:", Edad);
+        }
+        public Persona(SerializationInfo info, StreamingContext context)
+        {
+            Nombre = (string)info.GetValue("Name", typeof(string));
+            Apellido = (string)info.GetValue("LastName", typeof(string));
+            Edad = (string)info.GetValue("Age", typeof(string));
+
+        }
+    }
+
+
+}
---
+++ Serializacion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Serializacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion;
+            Persona GESTORPERSONA = new Persona();
+            Console.WriteLine("Ingrese entre una de las siguientes opciones:\n1.- Crear Persona \n2.-Mostrar todas las personas \n3.-Cargar Archivo \n4.- Serializar\n5.- Salir del programa\n");
+            opcion=Console.ReadLine();
+            while(opcion!="5")
+            {   
+                if (opcion == "1")
+                {
+                    Console.WriteLine("Escriba el nombre de la persona a ingresar:\n");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Escriba el apellido de la persona a ingresar:\n");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("Escriba la edad de la persona a ingresar:\n");
+                    string edad = Console.ReadLine();
+
+                    Persona persona = new Persona(name, LastName, edad);
+
+                    GESTORPERSONA.AddPerson(persona);
+
+                    Console.WriteLine("Persona Agregada con exito!.");
+                }
+                else if (opcion == "2")
+                {
+                    GESTORPERSONA.ShowListPerson();
+                }
+                else if (opcion == "3")
+                {
+
+
+                }
+                else if (opcion == "4")
+                {
+                    Stream stream = File.Open("PersonData.dat", FileMode.Create);
+
+                    BinaryFormatter bf = new BinaryFormatter();
+
+                    foreach (Persona persona in GESTORPERSONA.ReturnListPerson())
+                    {
+                        bf.Serialize(stream, persona);
+
+                    }
+
+                    stream.Close();
+                }
+                Console.WriteLine("Ingrese entre una de las siguientes opciones:\n1.- Crear Persona \n2.-Mostrar todas las personas \n3.-Cargar Archivo \n4.- Serializar\n5.- Salir del programa\n");
+                opcion = Console.ReadLine();
+            }
+            
+           
+
+        }
+    }
+}
